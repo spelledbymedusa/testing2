dc16c95ca41ad0c7bc6f4d53fec4361561999391
spelledbymedusa <tessathejedi@gmail.com>
2026-01-21 09:42:11 +0100

    Add App component with AppShell and HomePage

---
+++ src/app/App.tsx
@@ -0,0 +1,10 @@
+import { AppShell } from "./AppShell";
+import { HomePage } from "../pages/HomePage";
+
+export function App() {
+  return (
+    <AppShell>
+      <HomePage />
+    </AppShell>
+  );
+}
